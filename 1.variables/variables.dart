ac96f67147e4d5a68586ad942372bbc8fc109731
German Herrera <geherrera@unillanos.edu.co>
2021-09-04 08:15:37 -0500

    agrega ejemplo final -constante

--- 1.variables/variables.dart
+++ 1.variables/variables.dart
@@ -4,8 +4,16 @@ void main(){
   var edad = 89;
   int edad_dos = 34;
   var test;
-
   test = 100;
+
+  //null safety
+  int? variableNull = null;
+  String? variableStringNull = null;
+
+  //final - constante
+  final String variableFinalTest = "hola";
+  const String variableConstanteTest = "hola";
+
   print(nombre_dos.toUpperCase());
   print(nombre);
   print(test);
