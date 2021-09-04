73dbb39f8f25000e6caecf94aa1fd7751c4ebbe1
German Herrera <geherrera@unillanos.edu.co>
2021-09-04 10:54:59 -0500

    ejercicio operaciones

---
+++ 4.operadores/operadores.dart
@@ -0,0 +1,11 @@
+void main(){
+  var variable1 = 5;
+  var variable2 = 4.0;
+  
+  var variable3 = variable1 > 5 ? 'si' : 'no';
+  var variable4;
+  var variable5 = variable4 ?? "variable5 toma este valor";
+
+  print(variable5);
+
+}
