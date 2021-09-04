5dbf8e36cbd81312e5508e5a8b1ac6f28b1324aa
German Herrera <geherrera@unillanos.edu.co>
2021-09-04 07:57:26 -0500

    Ejercicio variables dart

---
+++ 1.variables/variables.dart
@@ -0,0 +1,14 @@
+void main(){
+  var nombre = "juan";
+  String nombre_dos = "juan";
+  var edad = 89;
+  int edad_dos = 34;
+  var test;
+
+  test = 100;
+  print(nombre_dos.toUpperCase());
+  print(nombre);
+  print(test);
+
+ 
+}
